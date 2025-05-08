c07827562b77c5b223a3ccd8244f3a80fb275c92
shubham444yt <st513279@gmail.com>
2025-05-08 11:29:52 +0530

    BlogDetails code is pushed

--- src/app/blog-d10/page.jsx
+++ src/app/blog-d10/page.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Navbar from '../../../Student-Alliance/src/components/Navbar'
 import BlogsDetail10 from '@/components/blog/BlogDetail10'
-import Footer from '../../../Student-Alliance/src/components/Footer'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
 
 const Page = () => {
   return (
     <div>
         <Navbar/>
         <BlogsDetail10/>
-        <Footer/> 
+        <Footer/>
     </div>
   )
 }
