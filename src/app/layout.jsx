2de2f7fda5d40241c729749c89df088257335efc
shubham444yt <st513279@gmail.com>
2025-06-03 12:14:59 +0530

    EDIT in image navbar check feature

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutClientWrapper from "@/components/LayoutClientWrapper";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+         {/* Google tag (gtag.js) */}
+<Script 
+ src="https://www.googletagmanager.com/gtag/js?id=G-L48FSWY2HB"
+ strategy="afterInteractive">
+  
+ </Script>
+<Script id="ga-init" strategy="afterInteractive"> 
+ {
+  `
+   window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L48FSWY2HB');
+  `
+ }
+</Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <LayoutClientWrapper>{children}</LayoutClientWrapper>
+
+        
       </body>
     </html>
   );
